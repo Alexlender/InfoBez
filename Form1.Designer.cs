600fe930d7c36d4b84caaf42ea58db2e8d645d08
Alexlender <sashacharugin@mail.ru>
2023-03-10 18:35:44 +0700

    complete

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,16 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textInput = new System.Windows.Forms.TextBox();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textOutput = new System.Windows.Forms.TextBox();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
-            this.button1 = new System.Windows.Forms.Button();
+            this.selectedMode = new System.Windows.Forms.ComboBox();
+            this.keyInput = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -42,60 +47,68 @@
             this.splitContainer2.Panel1.SuspendLayout();
             this.splitContainer2.Panel2.SuspendLayout();
             this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.keyInput)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // textInput
             // 
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Location = new System.Drawing.Point(0, 0);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(40, 37, 40, 37);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(533, 303);
-            this.textBox1.TabIndex = 0;
+            this.textInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textInput.Location = new System.Drawing.Point(0, 0);
+            this.textInput.Margin = new System.Windows.Forms.Padding(40, 37, 40, 37);
+            this.textInput.Multiline = true;
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(499, 314);
+            this.textInput.TabIndex = 0;
+            this.textInput.TextChanged += new System.EventHandler(this.textInput_TextChanged);
             // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.textInput);
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.textBox2);
-            this.splitContainer1.Size = new System.Drawing.Size(1067, 303);
-            this.splitContainer1.SplitterDistance = 533;
+            this.splitContainer1.Panel2.Controls.Add(this.textOutput);
+            this.splitContainer1.Size = new System.Drawing.Size(1000, 314);
+            this.splitContainer1.SplitterDistance = 499;
             this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 1;
             this.splitContainer1.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitContainer1_SplitterMoved);
             // 
-            // textBox2
+            // textOutput
             // 
-            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox2.Location = new System.Drawing.Point(0, 0);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(529, 303);
-            this.textBox2.TabIndex = 1;
+            this.textOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textOutput.Location = new System.Drawing.Point(0, 0);
+            this.textOutput.Margin = new System.Windows.Forms.Padding(4);
+            this.textOutput.Multiline = true;
+            this.textOutput.Name = "textOutput";
+            this.textOutput.ReadOnly = true;
+            this.textOutput.Size = new System.Drawing.Size(496, 314);
+            this.textOutput.TabIndex = 1;
             // 
             // splitContainer2
             // 
             this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer2.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.splitContainer2.Margin = new System.Windows.Forms.Padding(4);
             this.splitContainer2.Name = "splitContainer2";
             this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
             // splitContainer2.Panel1
             // 
-            this.splitContainer2.Panel1.Controls.Add(this.button1);
+            this.splitContainer2.Panel1.Controls.Add(this.button);
+            this.splitContainer2.Panel1.Controls.Add(this.progressBar);
+            this.splitContainer2.Panel1.Controls.Add(this.selectedMode);
+            this.splitContainer2.Panel1.Controls.Add(this.keyInput);
+            this.splitContainer2.Panel1.Controls.Add(this.label1);
             this.splitContainer2.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
             // 
             // splitContainer2.Panel2
@@ -103,33 +116,80 @@
             this.splitContainer2.Panel2.Controls.Add(this.splitContainer1);
             this.splitContainer2.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.splitContainer2.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.splitContainer2.Size = new System.Drawing.Size(1067, 554);
-            this.splitContainer2.SplitterDistance = 246;
+            this.splitContainer2.Size = new System.Drawing.Size(1000, 582);
+            this.splitContainer2.SplitterDistance = 263;
             this.splitContainer2.SplitterWidth = 5;
             this.splitContainer2.TabIndex = 2;
             // 
-            // button1
+            // selectedMode
             // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.button1.Location = new System.Drawing.Point(0, 206);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(1067, 40);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Сделать";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.selectedMode.FormattingEnabled = true;
+            this.selectedMode.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.selectedMode.Items.AddRange(new object[] {
+            "Кодировать",
+            "Декодировать"});
+            this.selectedMode.Location = new System.Drawing.Point(33, 124);
+            this.selectedMode.Name = "selectedMode";
+            this.selectedMode.Size = new System.Drawing.Size(121, 24);
+            this.selectedMode.TabIndex = 6;
+            // 
+            // keyInput
+            // 
+            this.keyInput.Location = new System.Drawing.Point(33, 71);
+            this.keyInput.Name = "keyInput";
+            this.keyInput.Size = new System.Drawing.Size(49, 22);
+            this.keyInput.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(29, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ключ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button
+            // 
+            this.button.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button.Location = new System.Drawing.Point(0, 195);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(1000, 58);
+            this.button.TabIndex = 0;
+            this.button.Text = "Сделать";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar.Location = new System.Drawing.Point(0, 253);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(1000, 10);
+            this.progressBar.Step = 1;
+            this.progressBar.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(1000, 582);
             this.Controls.Add(this.splitContainer2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(600, 500);
             this.Name = "Form1";
             this.Text = "Шифр Цезаря";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
@@ -137,20 +197,27 @@
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
             this.splitContainer2.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
             this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.keyInput)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textInput;
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.SplitContainer splitContainer2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textOutput;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown keyInput;
+        private System.Windows.Forms.ComboBox selectedMode;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,26 +5,93 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace InfoBez
 {
+    enum Mode { Encode, Decode };
+    
     public partial class Form1 : Form
     {
+        string outputText;
         public Form1()
         {
             InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            backgroundWorker1.ProgressChanged += backgroundWorker1_ProgressChanged;
+            backgroundWorker1.RunWorkerCompleted += backgroundWorker1_WorkDone;
+        }
+
+        private void Cesar(TextBox input, TextBox output, int key, Mode mode = Mode.Encode)
+        {
+           backgroundWorker1.RunWorkerAsync(new List<object>{ input.Text, key, mode});
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Cesar(textInput, textOutput, (int)keyInput.Value, (Mode)selectedMode.SelectedIndex);
         }
 
         private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
         {
 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            selectedMode.SelectedIndex = (int)Mode.Encode;
+
+            textInput.DoubleClick += TexBox_Click;
+            textOutput.DoubleClick += TexBox_Click;
+        }
+
+        private void TexBox_Click(object sender, EventArgs e)
+        {
+            (textInput.Text, textOutput.Text) = (textOutput.Text, textInput.Text);
+        }
+
+        private void textInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            //Cesar(textInput, textOutput, (int)keyInput.Value, (Mode)selectedMode.SelectedIndex);
+            List<object> list = e.Argument as List<object>;
+            string inputText = (string)list[0];
+            int key = (int)list[1];
+            Mode mode = (Mode)list[2];
+            outputText = "";
+            for (int i = 0; i<inputText.Length;i++)
+            {
+                char c = inputText[i];
+                outputText += (char)(c + key * (mode == Mode.Encode ? 1 : -1));
+                worker.ReportProgress((int)((double)i/inputText.Length*100));
+
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            textOutput.Text = outputText;
+            progressBar.Value = (int)e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_WorkDone(object sender, RunWorkerCompletedEventArgs e)
+        {
+            textOutput.Text = outputText;
+            progressBar.Value = 100;
         }
     }
 }
