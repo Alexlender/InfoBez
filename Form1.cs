551c8eed2717ea8a48ea03a6913b30558ab0806a
Alexlender <sashacharugin@mail.ru>
2023-04-10 00:56:19 +0700

    all

--- Form1.cs
+++ Form1.cs
@@ -14,25 +14,26 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.Remoting.Messaging;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Runtime.CompilerServices;
+using System.Reflection.Emit;
+using System.Reflection;
 
 namespace InfoBez
 {
 
     public partial class Form1 : Form
     { 
-        string outputText;
+        private Socket s;
         public Form1()
         {
             InitializeComponent();
-            backgroundWorker1.WorkerReportsProgress = true;
-            backgroundWorker1.WorkerSupportsCancellation = true;
-            backgroundWorker1.ProgressChanged += backgroundWorker1_ProgressChanged;
-            backgroundWorker1.RunWorkerCompleted += backgroundWorker1_WorkDone;
 
             IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 0);
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             socket.Bind(ipPoint);
-            
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             status.Text = $"Текущий порт: {((IPEndPoint)socket.LocalEndPoint).Port}";
             Task.Run(() => ConnectionHandlerAsync(socket));
         }
@@ -47,7 +48,7 @@ namespace InfoBez
                 var result = MessageBox.Show($"Принято входящее подключение\nIP {((IPEndPoint)client.RemoteEndPoint).Address}",
                     "ОБНАРУЖЕНА ПОПЫТКА ПРОНИКНОВЕНИЯ", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
                 if (result == DialogResult.Cancel)
-                    client.Close();
+                    client.Disconnect(true);
                 else
                 {
                     AddLog($"Выполнено подключение к {((IPEndPoint)client.RemoteEndPoint).Address}");
@@ -61,64 +62,93 @@ namespace InfoBez
         {
             try
             {
-                long key1 = long.Parse(openKey.Text);
-                long key2 = long.Parse(closeKey.Text);
-                await Logic(socket, key1, key2);
+                BigInteger key1 = BigInteger.Parse(openKey.Text);
+                BigInteger key2 = BigInteger.Parse(closeKey.Text);
+                await LogicAsync(socket, key1, key2);
             }
-            catch
+            catch (FormatException)
             {
                 MessageBox.Show("Вместо ключей написан мусор, всё сломалось!", "Что-то пошло не так!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-        }
-
-        private async Task Logic(Socket socket, long openKey, long closeKey)
-        {
-            var SendBytes = BitConverter.GetBytes(openKey);
-            await socket.SendAsync(new ArraySegment<byte>(SendBytes, 0, SendBytes.Length), SocketFlags.None);
-            var response = new byte[512];
-
-            socket.Receive(response);
-            int anotherKey = BitConverter.ToInt32(response, 0);
-            AddLog($"Открытый ключ соседа: {anotherKey}\n");
-            if(anotherKey == openKey)
+            catch (Exception ex)
             {
-                AddLog($"Ключи совпадают, всё хорошо\n");
+                MessageBox.Show($"{ex.Message}", "О Нет!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            else
-            {
-                AddLog($"Ключи разные, щас их перемножим и найдём ближайшее большее просто число\n");
-                openKey = KeyGen(anotherKey * openKey).Result;
-                AddLog($"Найденный ключ: {openKey}\n");
-            }
-
         }
 
-
-        private async Task<long> KeyGen(long row)
+        private async Task LogicAsync(Socket socket, BigInteger openKey, BigInteger closeKey)
         {
-
-            for (int i = 0; !(await CheckPrime(row)); i++)
-                row += i;
-            return row;
-        }
-
-
-
-        /// <returns>True if x is prime</returns>
-        private async Task<bool> CheckPrime(long x)
-        {
-            return await Task.Run(() =>
+            await Task.Run(() =>
             {
-                long sqrt = (int)Math.Sqrt(x);
-                if (sqrt * sqrt == x)
-                    return false;
-                for (long i = 2; i < sqrt; i++)
-                    if (x % i == 0)
-                        return false;
-                return true;
+                socket.SendAsync(new ArraySegment<byte>(openKey.ToByteArray()), SocketFlags.None);
+
+                var response = new byte[512];
+                socket.Receive(response);
+                BigInteger anotherKey = new BigInteger(response);
+                AddLog($"Общий открытый ключ у соседа: {anotherKey}\n");
+                if (anotherKey == openKey)
+                {
+                    AddLog($"Ключи совпадают, всё хорошо\n");
+                }
+                else
+                {
+                    AddLog($"Ключи разные, щас их перемножим и найдём ближайшее большее просто число\n");
+                    openKey = KeyGen(anotherKey * openKey);
+                    AddLog($"Найденный ключ: {openKey}\n");
+                }
+                var a = new BigInteger(CustomHash(closeKey, openKey));
+                AddLog($"Мой открытый ключ: {a}\n");
+                socket.SendAsync(new ArraySegment<byte>(a.ToByteArray()), SocketFlags.None);
+
+                var response2 = new byte[512];
+                socket.Receive(response2);
+                BigInteger b = new BigInteger(response2);
+                AddLog($"Открытый ключ соседа: {b}\n");
+
+                var key = new BigInteger(CustomHash(a, b));
+                AddLog($"Получился ключ: {key}\n");
+                socket.Disconnect(true);
+                AddLog($"Подключение закрыто\n\n");
+
             });
 
-            
+        }
+
+        public static BigInteger Sqrt(BigInteger n)
+        {
+            if (n == 0) return 0;
+            if (n > 0)
+            {
+                int bitLength = Convert.ToInt32(Math.Ceiling(BigInteger.Log(n, 2)));
+                BigInteger root = BigInteger.One << (bitLength / 2);
+
+                while (!isSqrt(n, root))
+                {
+                    root += n / root;
+                    root /= 2;
+                }
+
+                return root;
+            }
+
+            throw new ArithmeticException("NaN");
+        }
+
+        private static bool isSqrt(BigInteger n, BigInteger root)
+        {
+            BigInteger lowerBound = root * root;
+            BigInteger upperBound = (root + 1) * (root + 1);
+
+            return (n >= lowerBound && n < upperBound);
+        }
+
+        private BigInteger KeyGen(BigInteger row)
+        {
+            if (row.IsEven)
+                row += 1;
+            for (int i = 0; !Miller.isPrime(row); i+=2)
+                row += i;
+            return row;
         }
 
         private void AddLog(string text)
@@ -131,34 +161,38 @@ namespace InfoBez
 
             genP();
             genK();
+            genKey2.Visible = isMagic;
+            genPublic2.Visible = isMagic;
+
 
         }
-        private Socket Connect(IPEndPoint iPEndPoint)
+        private void Connect(IPEndPoint iPEndPoint)
         {
-            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             // пытаемся подключиться используя URL-адрес и порт
-            var result = socket.ConnectAsync(iPEndPoint);
+            var result = s.ConnectAsync(iPEndPoint);
 
             result.Wait(5000);
 
-            if (socket.Connected)
+            if (!s.Connected)
             {
-                return socket;
-            }
-            else
-            {
-                socket.Close();
                 throw new SocketException();
             }
+
         }
 
         private void button_Click(object sender, EventArgs e)
         {
             try
             {
+                if (s.Connected)
+                {
+                    MessageBox.Show("Подключение уже выполнено", "ಠ_ಠ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
                 var data = CheckFields(ip.Text, port.Text);
                 AddLog($"Подключение к {data.Address}:{data.Port}...\n");
-                Socket s = Connect(data);
+                Connect(data);
                 AddLog("Подключение выполнено\n");
                 Logic(s);
             }
@@ -167,17 +201,13 @@ namespace InfoBez
                 MessageBox.Show("Не удалось подключиться :(", "Что-то пошло не так!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 AddLog("Подключение отклонено\n");
             }
-            catch
+            catch(Exception ex)
             {
-                MessageBox.Show("Айпи или порт указаны неверно!", "ОШИБКА ВСЁ НЕПРАВИЛЬНО!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show(ex.Message, "ОШИБКА ВСЁ НЕПРАВИЛЬНО!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 AddLog("Подключение отклонено\n");
             }
-
-
-
         }
 
-        
 
         private IPEndPoint CheckFields(string ip, string port)
         {
@@ -190,91 +220,31 @@ namespace InfoBez
 
     
 
-        private int CustomHash(int x)
+        private long CustomHash(BigInteger x, BigInteger p)
         {
-            return 13 ^ (x ^ 7) % 99999;
-        }
-
-        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
+            return (int)((p ^ x) % 2147483647);
         }
 
 
-        private void textInput_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
-        {
-            BackgroundWorker worker = sender as BackgroundWorker;
-            List<object> list = e.Argument as List<object>;
-            string inputText = (string)list[0];
-            string key = (string)list[1];
-
-            outputText = "";
-
-            int len;
-            if (inputText.Length > key.Length)
-                len = key.Length * (inputText.Length / key.Length + (inputText.Length % key.Length > 0 ? 1 : 0));
-            else
-                len = (int)key.Length;
-
-            inputText += new string(' ', len - inputText.Length);
-            key = String.Concat(Enumerable.Repeat(key, len / key.Length));
-
-            for(int i = 0; i< len; i++)
-            {
-                outputText += (char)(inputText[i] ^ key[i]);
-            }
-
-
-        }
-
-        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
-        {
-            textOutput.Text = outputText;
-            progressBar.Value = (int)e.ProgressPercentage;
-        }
-
-        private void backgroundWorker1_WorkDone(object sender, RunWorkerCompletedEventArgs e)
-        {
-            textOutput.Text = outputText;
-            progressBar.Value = 100;
-        }
-
-        private void key_KeyPress(object sender, KeyPressEventArgs e)
-        {
-
-        }
-
-        private void key_MouseEnter(object sender, EventArgs e)
-        {
-            
-
-        }
-
-
-        private void splitContainer2_Panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
 
         private void genPublic_Click(object sender, EventArgs e)
         {
             genP();
         }
 
+        private BigInteger Rand(uint len)
+        {
+            var rng = new RNGCryptoServiceProvider();
+            byte[] bytes = new byte[len];
+            rng.GetBytes(bytes);
+
+            return BigInteger.Abs(new BigInteger(bytes));
+        }
+
         private void genP()
         {
             var rand = new Random();
-            openKey.Text = KeyGen(rand.Next()).Result.ToString();
+            openKey.Text = (KeyGen(rand.Next(10000,int.MaxValue))).ToString();
         }
 
         private void genKey_Click(object sender, EventArgs e)
@@ -288,5 +258,114 @@ namespace InfoBez
             closeKey.Text = rand.Next(0,10000).ToString();
         }
 
+
+        private static bool isMagic = false;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control)
+            {
+                isMagic = !isMagic;
+                DoMagic(isMagic);
+            }
+        }
+
+        private async void DoMagic(bool isMagic)
+        {
+            foreach (var label in splitContainer2.Panel1.Controls.OfType<System.Windows.Forms.Label>().ToList())
+            {
+                label.ForeColor = isMagic ? Color.Green : Color.Black;
+            }
+
+
+            foreach (var textable in splitContainer2.Panel1.Controls.OfType<System.Windows.Forms.Label>().Cast<Control>().ToList().Union(
+                splitContainer2.Panel1.Controls.OfType<TextBox>().Cast<Control>().ToList().Append(textOutput).Append(button)))
+            {
+
+                textable.ForeColor = isMagic ? Color.Green : Color.Black;
+                textable.Font = isMagic ? new Font("Lucida Console", textable.Font.Size) : new Font("Microsoft Sans Serif", textable.Font.Size);
+
+                if(textable is TextBox)
+                    textable.BackColor = isMagic ? Color.FromName("ControlDark") : Color.FromName("Control");
+            }
+
+            splitContainer2.Panel1.BackColor = !isMagic ? Color.FromName("Control") : Color.Black;
+
+            textOutput.BackColor = splitContainer2.Panel1.BackColor = !isMagic ? Color.FromName("Control") : Color.Black;
+            textOutput.ForeColor = isMagic ? Color.White : Color.Black;
+
+            BackColor = !isMagic ? Color.FromName("Control") : Color.Black;
+
+            genKey1.Visible = !isMagic;
+            genPublic1.Visible = !isMagic;
+            genKey2.Visible = isMagic;
+            genPublic2.Visible = isMagic;
+
+            foreach (var field in new List<TextBox>{ openKey, closeKey })
+            {
+                field.Multiline = isMagic;
+                field.Size = new Size(field.Size.Width, isMagic ? 148 : 23);
+            }
+
+        }
+
+        private void genPublic2_Click(object sender, EventArgs e)
+        {
+            Work(genPublic2);
+            genP2();
+            
+        }
+
+        private void genKey2_Click(object sender, EventArgs e)
+        {
+            genK2();
+        }
+
+        private async void genP2()
+        {
+            var rand = new Random();
+            await Task.Run(() => {
+                openKey.Text = KeyGen(Rand(64)).ToString();
+                StopWork();
+            });
+            
+        }
+
+        bool isWorking = false;
+
+        private void StopWork()
+        {
+            isWorking = false;
+        }
+        private async void Work(Button button)
+        {
+            isWorking = true;
+            await Task.Run(async () =>
+            {
+
+                for (int i = 0; isWorking; i = i == 3 ? 0 : i + 1)
+                {
+                    button.Text = "Думаю" + new string('.', i);
+                    await Task.Delay(400);
+                }
+
+                button.Text = "Генерировать";
+
+
+            });
+        }
+
+        private void genK2()
+        {
+            var rand = new Random();
+            closeKey.Text = Rand(64).ToString();
+        }
+
+        private void textOutput_Clear(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show($"Отчистить лог?",
+                    "УДАЛИТЬ?", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (result == DialogResult.OK)
+                textOutput.Text = "";
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace InfoBez
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
-        [STAThread]
+        [MTAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
